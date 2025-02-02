0d5abb553296f18fb1d7a163816f72d0c414adcc
indu1803 <eindu1989@gmail.com>
2025-02-02 21:14:53 +0530

    1st commit

---
+++ src/test/java/cmd/org/ComamandPromptTestng.java
@@ -0,0 +1,12 @@
+package cmd.org;
+
+import org.testng.annotations.Test;
+
+public class ComamandPromptTestng {
+
+	@Test
+	public void tc01()
+	{
+		System.out.println("Maven run under command prompt");
+	}
+}
